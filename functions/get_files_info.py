483e5898d74e7d458b897e27db6185cc9e122795
Dave Oleksy <dave@doleksy.com>
2025-07-23 14:21:29 -0500

    finished CH2: L2: Get Files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,14 +1,10 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    print(f"wd: {working_directory}, dir: {directory}")
-    
     target = os.path.join(working_directory, directory)
     target_abs = os.path.abspath(target)
-    print(f"target: {target_abs}")
 
     working_abs = os.path.abspath(working_directory)
-    print(f"working: {working_abs}")
 
     if not target_abs.startswith(working_abs):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
@@ -16,11 +12,21 @@ def get_files_info(working_directory, directory="."):
     if not os.path.isdir(target_abs):
         return f'Error: "{directory}" is not a directory'
 
-    print(os.listdir(target_abs))
     contents_str = ""
-    for entry in os.listdir(target_abs):
+    try:
+        dir_listing = os.listdir(target_abs)
+    except Exception as e:
+        return f"Error: in os.listdir({target_abs}): {e}"
+    
+    for entry in dir_listing:
         entry_abs = os.path.join(target_abs, entry)
-        contents_str += f"- {entry}: file_size={os.path.getsize(entry_abs)} bytes, is_dir={os.path.isdir(entry_abs)}\n"
+
+        try:
+            entry_size = os.path.getsize(entry_abs)
+        except OSError as e:
+            return f"Error: in os.path.getsize({entry_abs}): {e}"
+        
+        contents_str += f" - {entry}: file_size={entry_size} bytes, is_dir={os.path.isdir(entry_abs)}\n"
 
     return contents_str
 
